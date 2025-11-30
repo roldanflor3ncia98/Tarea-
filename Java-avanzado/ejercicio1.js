4adfe5a2a4bb7abb5dd8b15ed58cd6bb19af72ec
roldanflor3ncia98 <roldanlflorencia98@gmail.com>
2025-11-30 04:35:51 -0300

    agrego carpeta java avanzado

---
+++ Java-avanzado/ejercicio1.js
@@ -0,0 +1,12 @@
+function calcularPromedio(nota1, nota2) {
+    const promedio = (nota1 + nota2) / 2
+    return promedio
+}
+
+const nota1 = 7
+const nota2 = 5
+const promediofinal = calcularPromedio(nota1, nota2);
+console.log(promediofinal);
+console.log(`La nota final de cursada es ${promediofinal}`);
+
+
